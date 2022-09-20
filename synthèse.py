b0ccc1c7536b471f41d5a7f3227508db1b8dec91
gmpommier <113106639+gmpommier@users.noreply.github.com>
2022-09-20 08:47:52 +0200

    Add files via upload

---
+++ synthèse.py
@@ -0,0 +1,32 @@
+"""
+a=4
+b=3.2
+c="hello"
+print(a,b,c,end="") # pas de retour à la ligne
+print(a+5) # affichage de variable avec retour à la ligne
+print("bonjour") #affichage de texte avec retour à la ligne
+a=
+if a>15:
+    print("bien")
+else:
+    print("non")
+    print("inférieur")
+a = 2
+b = 3
+if a == 2 and b == 3:
+    print("ok")
+else:
+    print("non")
+a=4
+b=5
+if a==3 and b==6:
+    print("égalité")
+else:
+    print("pas  égal")
+
+
+"""
+n=0
+while n<1000000000000:
+     print(n,end=",")
+     n=n+2000000000000
